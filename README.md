0e5e0f4959be4af5c5ed2c583d12605c62d6ae53
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2024-03-21 17:05:57 +0530

    minor changes

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # myfitnesspal-grafana
+This Project is based on flutter. User will be able to generate Grafana Dashboard using CSV file generated from MyFitnessPal Android App.
+
+Note: Sample data file has been uploaded to data Folder. This file has been downloaded from some website providing user an sample csv file for MyFitnessPal Android App.
--- fitnessgrafanashelf/lib/main.dart
+++ fitnessgrafanashelf/lib/main.dart
@@ -142,11 +142,13 @@ Future<Response> _uploadCsvHandler(Request request) async {
 void main(List<String> args) async {
   // Use any available host or container IP (usually `0.0.0.0`).
   final ip = InternetAddress.anyIPv4;
-
+  final overrideHeaders = {
+    ACCESS_CONTROL_ALLOW_ORIGIN: '*',
+  };
   // Configure a pipeline that logs requests.
   final handler = Pipeline()
       .addMiddleware(logRequests())
-      .addMiddleware(corsHeaders())
+      .addMiddleware(corsHeaders(headers: overrideHeaders))
       .addHandler(_router);
 
   // For running in containers, we respect the PORT environment variable.
